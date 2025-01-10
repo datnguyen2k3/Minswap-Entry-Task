85a9476c57f4667a7bf4b8e000d66acd4ec1a109
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-01-10 11:39:21 +0700

    update file main

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
 import {getLucidInstance} from "./lucid-instance";
+import {LucidEvolution} from "@lucid-evolution/lucid";
 
 const main = async () => {
     const lucid = await getLucidInstance();
     const seedPhrase = "urge chuckle print picture behind hat client ask sword payment uncover equip alert rely remove crash letter grunt edit twenty test ecology museum dry";
     lucid.selectWallet.fromSeed(seedPhrase);
 
-    const address = await lucid.wallet().address(); // Bech32 encodedaddress
-    console.log("Address: ", address);
+}
 
-    // wallet balance
-    console.log(address);
+const getAddresses = async (lucid: LucidEvolution) => {
+    const addresses = await lucid.wallet().address();
+    console.log("Addresses: ", addresses);
+}
+
+const getBalance = async (lucid: LucidEvolution) => {
     const utxos = await lucid.wallet().getUtxos();
     console.log("UTxOs: ", utxos);
+}
 
-    // get reward balance
-    const delegation = await lucid.wallet().getDelegation();
-    console.log("Delegation: ", delegation);
-
-    // submit transaction
+const submitTx = async (lucid: LucidEvolution) => {
     const receive_address = 'addr_test1qqlr0955spgj8qx4dr4a3atsfgmg6957uyg33lfgfnvfyv4h3mgagzw5nzmlqw5mq386hvffyxrqhqve8pjn8ddnh55sy2kqx9'
     const amount = 1000000;
     const tx = await lucid
