d44ac7ff6545edf02c94e5ad5b13cde38f80807e
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-01-14 14:09:52 +0700

    add unlock for vesting.ts

--- hello-world/vesting.ts
+++ hello-world/vesting.ts
@@ -1,6 +1,17 @@
-import {Data} from "@lucid-evolution/lucid";
+import {Constr, Data, UTxO} from "@lucid-evolution/lucid";
 import {lock_assets} from "./lock";
-import {getPublicKeyHash, getScriptsAddress, toCBOR} from "./common";
+import {
+    getPrivateKey,
+    getPublicKeyHash,
+    getScriptsAddress,
+    getSpendingValidator,
+    isCantCastCborToObject, submitTx,
+    toCBOR,
+    toObject
+} from "./common";
+import {getLucidOgmiosInstance} from "../src/lucid-instance";
+import {unlock_assets} from "./unlock";
+import {utf8ToHex} from "../src/ultis/ultis";
 
 const DatumVestingScheme = Data.Object({
     lock_until: Data.Integer(),
@@ -11,7 +22,7 @@ const VestingValidatorIndex = 2;
 
 export async function lockAssetsToVestingScriptsAddress(amount: bigint): Promise<void> {
     const vestingScriptAddress = getScriptsAddress(VestingValidatorIndex);
-    lock_assets(
+    await lock_assets(
         vestingScriptAddress,
         amount,
         toCBOR(
@@ -24,10 +35,68 @@ export async function lockAssetsToVestingScriptsAddress(amount: bigint): Promise
     );
 }
 
-export async function unlockAssetsToVestingScriptsAddress(receiveAddress: string): Promise<void> {
+export async function isValidVestingScriptAddressUTxO(utxo: UTxO, publicKeysHash: string): Promise<boolean> {
+    if (!utxo.datum) {
+        return false;
+    }
+    const datum = toObject(utxo.datum, DatumVestingScheme);
+    return datum.lock_until < BigInt(Date.now()) && datum.beneficiary === publicKeysHash;
+}
 
+export async function getVestingScriptAddressUTxOs(): Promise<UTxO[]> {
+    const scriptAddress = getScriptsAddress(VestingValidatorIndex);
+    const lucid = await getLucidOgmiosInstance();
+    const scriptsAddressUtxos = await lucid.utxosAt(scriptAddress);
+    const publicKeyHash = await getPublicKeyHash();
+    const utxos: UTxO[] = [];
+
+    for (const utxo of scriptsAddressUtxos) {
+        try {
+            if (await isValidVestingScriptAddressUTxO(utxo, publicKeyHash)) {
+                utxos.push(utxo);
+            }
+        } catch (e) {
+            if (isCantCastCborToObject(e)) {
+                continue;
+            }
+            console.error("Error parsing datum:", e);
+        }
+    }
+
+    console.log("Vesting script address UTxOs:", utxos);
+    return utxos;
+}
+
+export async function unlockAssetsToVestingScriptsAddress(): Promise<void> {
+    const utxos = await getVestingScriptAddressUTxOs();
+    const receiveAddress = "addr_test1vpfsn7ncdptvzf3dp9dcnt0kfl522f266xg59jw9xu6eusgmessnp"
+    const spendValidator = getSpendingValidator(VestingValidatorIndex);
+    const scriptsAddress = getScriptsAddress(VestingValidatorIndex);
+    const redeemer = Data.to(new Constr(0, [utf8ToHex("Hello, World!")]));
+    const amount = BigInt(1000000);
+
+    const lucid = await getLucidOgmiosInstance();
+    lucid.selectWallet.fromPrivateKey(getPrivateKey());
+
+    const tx = await lucid
+        .newTx()
+        .collectFrom(utxos, redeemer)
+        .addSigner(await lucid.wallet().address())
+        .validFrom(Date.now() - 100000000)
+        .validTo(Date.now() + 100000000)
+        .pay.ToAddress(
+            receiveAddress,
+            {lovelace: BigInt(amount)}
+        )
+        .attach.SpendingValidator(spendValidator)
+        .complete();
+
+    await submitTx(tx, lucid);
 }
 
 async function main(): Promise<void> {
-    await lockAssetsToVestingScriptsAddress(BigInt(1000000));
+    // await lockAssetsToVestingScriptsAddress(BigInt(1000000));
+    // await unlockAssetsToVestingScriptsAddress();
 }
+
+main().then(r => console.log("Done!")).catch(e => console.error(e));
