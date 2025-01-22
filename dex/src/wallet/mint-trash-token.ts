ff215613f275b2706fe6cbc1dc1ffb7d89065c5f
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-01-22 17:43:18 +0700

    add mint trash token

---
+++ dex/src/wallet/mint-trash-token.ts
@@ -0,0 +1,28 @@
+import {getLucidOgmiosInstance} from "../../../src/lucid-instance";
+import {readMintValidators} from "../utils";
+import {PLUTUS_PATH, PRIVATE_KEY_PATH} from "../types";
+import {Constr, Data, fromText} from "@lucid-evolution/lucid";
+import {getPrivateKeyFrom, submitTx} from "../../../hello-world/common";
+
+export async function mintTrashToken(privateKey: string) {
+    const lucid = await getLucidOgmiosInstance();
+    lucid.selectWallet.fromPrivateKey(privateKey);
+
+    const mintTrashValidator = readMintValidators(
+        "mint_trash_token.mint_trash_token.mint",
+        PLUTUS_PATH,
+        []
+    )
+    const trashAssetName = `${mintTrashValidator.policyId}${fromText("TRASH_TOKEN")}`;
+    const mintRedeemer = Data.to(new Constr(0, []));
+
+    const tx = await lucid
+        .newTx()
+        .mintAssets({[trashAssetName]: BigInt(1000000000)}, mintRedeemer)
+        .attach.MintingPolicy(mintTrashValidator.policyScripts)
+        .complete();
+
+    await submitTx(tx, lucid);
+}
+
+// mintTrashToken(getPrivateKeyFrom(PRIVATE_KEY_PATH)).then(() => console.log("Mint Trash Token successfully!"));
